7f1545926bf435c4c4fc0eb0adf487e97b3ed6bc
georgiy-g <grigoliya.g@gmail.com>
2021-12-10 14:36:06 +0530

    lessons 1

--- classwork/my-project/src/App.js
+++ classwork/my-project/src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      title: 'Some title',
+      user: {
+        name: 'Giya',
+        age: 32,
+        email: '',
+        test: {
+          value: 1,
+          count: 0
+        }
+      }
+    }
+
+    this.changeName = this.changeName.bind(this)
+  }
+
+  // state = {
+  //   title: 'Some title',
+  //   user: {
+  //     name: 'Giya',
+  //     age: 32,
+  //     email: ''
+  //   }
+  // }
+
+  incrementAge = (increment = 1, event) => {
+    this.setState({user: {...this.state.user, age: this.state.user.age + increment}})
+  }
+
+  changeName() {
+    this.setState({user: {...this.state.user, name: 'Dima'}})
+  }
+
+  render() {
+    console.log('render')
+    const {title, user} = this.state
+
+    return (
+      <div className="App">
+        <div>{title}</div>
+        <div>{user.name}</div>
+        <div>{user.age}</div>
+        <div>{user.test.value}</div>
+
+        {user.email.length > 0 && <div>{user.email}</div>}
+
+        {user.email.length > 0 ? <div>{user.email}</div> : 'No emails'}
+
+        {/* <div>----</div>
+        <div>{undefined}</div>
+        <div>{null}</div>
+        <div>{true}</div>
+        <div>{false}</div>
+        <div>{0}</div> */}
+
+        <button onClick={() => this.setState({title: 'New title'})}>Change title</button>
+        <button onClick={this.changeName}>Change name</button>
+        <button onClick={(e) => this.incrementAge(1, e)}>Increment age</button>
+        <button onClick={() => this.incrementAge(4)}>Increment age on 4 points</button>
+        <button onClick={() => this.setState((state) => ({user: {...state.user, age: state.user.age + 1 }}),() => console.log('age was changed') )}>Increment second age</button>
+
+        <button onClick={() => this.setState({user: {...this.state.user, test: {...this.state.user.test, value: this.state.user.test.value + 1}}})}>Increment value</button>
+        {/* <button onClick={() => this.state.title = "New title2"}>Change title</button> */}
+        {/* <button onClick={() => }>Increment age</button> */}
+      </div>
+    );
+  }
 }
-
-export default App;
